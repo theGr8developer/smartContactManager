236a029fe889df8fe9c105ef5a82f053be90a67c
theGr8developer <mdafroz.nitk@gmail.com>
2023-03-14 00:13:24 +0530

    signup form design

--- src/main/java/com/smart/smartcontactmanager/controller/HomeController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.smart.smartcontactmanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,14 +10,24 @@ public class HomeController {
 
     @GetMapping("/")
  
-    public String Home(){
-
+    public String Home(Model model){
+        model.addAttribute("title", "Home Smart Contact manager");
         return "home";
     }
     @GetMapping("/about")
-    public String About(){
-
+    public String About(Model model){
+        model.addAttribute("title", "About Smart Contact manager");
         return "about";
+    }
+    @GetMapping("/signup")
+    public String Signup(Model model){
+        model.addAttribute("title", "Signup Smart Contact manager");
+        return "signup";
+    }
+    @GetMapping("/login")
+    public String Login(Model model){
+        model.addAttribute("title", "Registration Smart Contact manager");
+        return "login";
     }
     
 }
